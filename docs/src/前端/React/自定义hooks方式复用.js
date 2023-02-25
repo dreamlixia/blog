ce081eafa26ec064497e7a4e59a39ec891b5db5f
dreamlixia <1763112943@qq.com>
2023-02-26 01:53:34 +0800

    简单自定义hook

---
+++ docs/src/前端/React/自定义hooks方式复用.js
@@ -0,0 +1,35 @@
+// 自定义hooks方式复用
+import React, { useEffect, useState } from 'react'
+
+function usePosition() {
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    const handleMouseMove = (e) => {
+        const { clientX, clientY } = e
+        setX(clientX)
+        setY(clientY)
+    }
+
+    useEffect(() => {
+        document.addEventListener('mousemove', handleMouseMove)
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove)
+        }
+    })
+
+    return [
+        {x, y}
+    ]
+}
+// 使用
+function Index() {
+    const [ position ] = usePosition()
+    return (
+        <div>
+            <p>x: {position.x}, y: {position.y}</p>
+        </div>
+    )
+}
+
+export default Index
