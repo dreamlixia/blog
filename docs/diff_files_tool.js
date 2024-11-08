dbbbda1ab8ce71f87c84f49c2ac9f56b4e8611ca
jinglixia <jing1763112943@163.com>
2024-11-08 16:43:21 +0800

    feat: 实现打印当前分支与master分支对比的所有diff文件列表

--- docs/diff_files_tool.js
+++ docs/diff_files_tool.js
@@ -1,3 +1,6 @@
+/**
+ * 实现打印当前分支与master分支对比的所有diff文件列表
+ */
 const { exec } = require('child_process');
 
 // 执行 Git 命令
@@ -23,7 +26,22 @@ const getCurrentBranch = async () => {
 
 // 获取当前分支和 master 分支之间的差异文件列表
 const getDiffFiles = async (currentBranch) => {
-  const diffFiles = await execGitCommand(`git diff --name-only ${currentBranch} master`);
+
+//   const diffFiles = await execGitCommand(`git diff --name-only ${currentBranch} master`);
+
+/**
+ * 关键点：
+ * 时间较久的分支会出现 diff 出来许多无用其他文件的问题，解决办法是需要寻找两个分支的共同祖先提交
+ * 
+ * const  base_commit =`$(git merge-base HEAD origin/master)`;
+ * const diffFiles = await execGitCommand(`git diff --relative --name-only ${base_commit}`);
+ * 
+ */
+ 
+  const  base_commit =`$(git merge-base HEAD origin/master)`;
+
+  const diffFiles = await execGitCommand(`git diff --relative --name-only ${base_commit}`);
+ 
   return diffFiles.split('\n').filter(file => file);
 };
 
@@ -33,7 +51,7 @@ const main = async () => {
     console.log(`当前分支: ${currentBranch}`);
 
     const diffFiles = await getDiffFiles(currentBranch);
-    console.log(`当前分支和 master 分支之间的差异文件列表:`);
+    console.log(`当前分支和 master 分支之间的差异文件列表共${diffFiles.length}个:`);
     diffFiles.forEach(file => console.log(file));
   } catch (error) {
     console.error(error);
--- docs/test_console.js
+++
@@ -1,7 +0,0 @@
-const a = 'a';
-
-console.log('a =', a);
-
-const b = a + '1';
-
-console.log('b =', b);
